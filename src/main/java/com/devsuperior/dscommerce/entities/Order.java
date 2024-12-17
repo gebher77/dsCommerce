74496919372b10fa79a1621ba59b16ca0e750194
Gabriel Melo <melo90350@gmail.com>
2024-12-17 20:11:20 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@
     import jakarta.persistence.*;
 
     import java.time.Instant;
+    import java.util.HashSet;
+    import java.util.List;
+    import java.util.Set;
 
 
     @Entity
@@ -24,6 +27,8 @@
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
         public Order() {
         }
@@ -75,4 +80,13 @@
         public void setPayment(Payment payment) {
             this.payment = payment;
         }
+
+        public Set<OrderItem> getItems() {
+            return items;
+        }
+
+        public List<Product> getProducts() {
+            return items.stream().map(x -> x.getProduct()).toList();
+        }
+
     }
